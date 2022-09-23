67988f150a6d744ca1fa15f5c7c70f79104e9265
Juacun <juanpablo.cundere@gmail.com>
2022-09-23 03:59:00 -0300

    Creation of UserController in controller package

---
+++ src/main/java/com/jpcode/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.jpcode.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/user")
+    public String getUserPage() {
+        return "menuUser";
+    }
+
+}
